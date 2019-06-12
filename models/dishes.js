280764091d6fbf2a6c8942f60104da59624a7dfc
Han Xu <hanx-2011@hotmail.com>
2019-06-11 22:22:38 -0700

    integrated express routes with mongodb server and mongoose models

---
+++ models/dishes.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+// define schema
+
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema] // can contain multiple comment documents
+},  
+{
+    usePushEach: true,
+    timestamps: true
+});
+
+// create model from schema. Mongoose auto converts singular name to plural
+var Dishes = mongoose.model('Dish', dishSchema); 
+
+module.exports = Dishes;
