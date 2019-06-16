072544ed751d08925b2e88bd6a82b2602e2f9364
Han Xu <hanx-2011@hotmail.com>
2019-06-16 16:31:54 -0700

    passport JWT authentication

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +17,7 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -31,65 +34,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
 
-// Define authentication middleware function
-function auth(req, res, next) {
+app.use(passport.initialize());
 
-    // if session does not exist for user
-    if (!req.session.user) { 
-        var authHeader = req.headers.authorization;
-    
-        // if no authentication header
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-
-        // obtain the headers on index 1. Output: 'username:password'
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64')         
-            .toString().split(':');
-        var username = auth[0];
-        var password = auth[1];
-
-        // handle authentication and set user permissions
-        if (username === 'admin' && password === 'password') {
-            req.session.user = 'admin'
-            next();
-        // if authentication info is wrong
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-    } else {
-        // if session exists for user
-        if (req.session.user === 'admin') {
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
-    }
-}
-
-app.use(auth);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // serve static data from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 // serve routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
 
 const Dishes = require('../models/dishes');
 
@@ -17,7 +19,8 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+// if authenticate successful, then proceed with function
+.post(authenticate.verifyUser, (req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log(`Dish Created `, dish);
@@ -27,11 +30,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch(err => next(err));
 }) 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403; 
     res.end(`PUT operation not supported on /dishes`)
 }) 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.remove({})
     .then((response) => {
         res.statusCode = 200;
@@ -52,11 +55,11 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403; 
     res.end(`POST operation not supported on /dishes/${req.params.dishId}`)
 }) 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Dishes.findByIdAndUpdate(
         req.params.dishId, 
         { $set: req.body }, 
@@ -69,7 +72,7 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch(err => next(err));
 }) 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((response) => {
         res.statusCode = 200;
@@ -97,7 +100,7 @@ dishRouter.route('/:dishId/comments')
     }, (err) => next(err))
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null) {
@@ -116,11 +119,11 @@ dishRouter.route('/:dishId/comments')
     }, (err) => next(err))
     .catch(err => next(err));
 }) 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403; 
     res.end(`PUT operation not supported on /dishes/${req.params.dishId}/comments`)
 }) 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null) {
@@ -163,11 +166,11 @@ dishRouter.route('/:dishId/comments/:commentId')
     }, (err) => next(err))
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403; 
     res.end(`POST operation not supported on /dishes/${req.params.dishId}/comments/${req.params.commentId}`);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -195,7 +198,7 @@ dishRouter.route('/:dishId/comments/:commentId')
     }, (err) => next(err))
     .catch(err => next(err));
 }) 
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
