75e5f20a5e9bca7d2e9c06a62b3aebacf5bb9816
Han Xu <Han.Xu@Nintex.com>
2019-06-18 13:17:15 -0700

    enforce unique names in mongoose schemas

--- models/promotions.js
+++ models/promotions.js
@@ -7,7 +7,8 @@ const Currency = mongoose.Types.Currency;
 const promotionSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     image: {
         type: String,
